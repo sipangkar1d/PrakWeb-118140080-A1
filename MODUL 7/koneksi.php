fe2fc03dce72644ab4355a5234f9f13615c73ae9
sipangkar1d <45484386+sipangkar1d@users.noreply.github.com>
2020-12-06 23:37:41 +0700

    Delete koneksi.php

--- MODUL 7/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$koneksi = mysqli_connect("localhost", "root", "", "prakweb3");
-if (mysqli_connect_error()) {
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}else{
-	return ($koneksi);
-	}
-
-?>
